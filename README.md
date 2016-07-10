69d4f30b88014c304bebbf7fb4faaee5d9d404fa
gnkatchmar <gunk55@msn.com>
2016-07-10 08:58:55 -0700

    before enhancement

--- README.md
+++ README.md
@@ -1,20 +1,26 @@
-# 201_Assign4
+# 201_Assignment 5 July 10, 2016
 
 Assignment for Class
-Today we will be expanding our javascript to include functions so that we can execute code blocks more logically, and later on, in a more controlled manner.
-
-Submit Your Work
+Today you will be further extending your guessing game by working with CSS and colors. Once complete you'll be working to deploy your code to the interwebs!
 
 User Stories (MVP)
 
-As a developer, I want my code written in a modular way, so that I can reuse functionality without duplicating code.
-As a developer, I want my user to see the answers and responses on the web page, so we do not have to constantly interact with alerts.
+As a developer, I want to further apply D.R.Y. to my code, so that I do not have duplicate and unnecessary code.
+As a developer, I want to add some basic styling to my site, so that I can increase the happiness and engagement of my users.
+As a developer, I want to include images in my game, when the user guesses correctly, so that the game is more informative and fun.
+As a developer, I want to color code the answers on-screen, so my user can visually see what answers were right or wrong.
+As a developer, I want to host my game online so that anyone can play!!
+
 Technical Requirements
 
-Updated and cleanly documented README
-Game functionality is encapsulated in functions
-Alerts have been removed from the game, and replaced by printing answers to the DOM.
+Proper syntax and refactoring when applying D.R.Y.
+Proper use of arrays and functions when applying DRY
+CSS stylesheet linked to html
+Basic CSS properties and values used, such as color, font-size, and font-family
+Images are correctly sized and styled
+Color properly applied on right/wrong answers
+GH-Pages deployment fully functional
+
 User Stories (Stretch)
 
-As a developer, I want to add a sixth question to my game, which has multiple possible answers (hint: using an array and for loop)
-As a developer, I want to add some basic styling to my site, so that I can increase the happiness and engagement of my users (hint: do not use inline styling for this; you should link to another file for your styling)
+As a developer, I want my site to look more professional, so my users will enjoy their experience (hint: Add more specific CSS styling to make more pretty)
--- game.js
+++ game.js
@@ -15,10 +15,10 @@ function Quiz(question, answer, elaborate) {
   if (guess != null) {
     
     if ((guess.charAt(0).toLowerCase() === answer)) {
-      document.getElementById("QuizContent").innerHTML += "<br>Correct! "+elaborate+"<br>";
+      document.getElementById("QuizContent").innerHTML += "<br />Correct! "+elaborate+"<br />";
       correct++;
     } else {
-     document.getElementById("QuizContent").innerHTML += "<br>Incorrect! "+elaborate+"<br>";}
+     document.getElementById("QuizContent").innerHTML += "<br />Incorrect! "+elaborate+"<br />";}
       
   } else {
     Cancel();
@@ -34,15 +34,15 @@ function NumQuiz () {
     var numGuess = prompt("Guess my favorite number. It is between 1 and 10:","");
     if (numGuess != null) {
       if (numGuess == 5) {
-        document.getElementById("QuizContent").innerHTML += "<br>Yes! You guessed my favorite number (because I was born on Cinco de Mayo).<br>";
+        document.getElementById("QuizContent").innerHTML += "<br />Yes! You guessed my favorite number (because I was born on Cinco de Mayo).<br />";
         numAttempts++;
         numQuizDone = true;
       } else {
         numAttempts++;
         if (numGuess < 5) {
-          document.getElementById("QuizContent").innerHTML += "<br>Too low; try again.<br>";
+          document.getElementById("QuizContent").innerHTML += "<br />Too low; try again.<br />";
         } else {
-          document.getElementById("QuizContent").innerHTML += "<br>Too high; try again.<br>";
+          document.getElementById("QuizContent").innerHTML += "<br />Too high; try again.<br />";
         }
       }
     } else {
@@ -53,7 +53,7 @@ function NumQuiz () {
 }
 
 function Cancel() {
-  document.getElementById("QuizContent").innerHTML += "<br>The game was cancelled.";
+  document.getElementById("QuizContent").innerHTML += "<br />The game was cancelled.";
   cancelled = "Y";
 }
 
@@ -61,7 +61,7 @@ function playGame() {
   document.getElementById("QuizStart").innerHTML = "Quiz Time!";
   GetUser();
   if (user != null) {
-    document.getElementById("QuizContent").innerHTML = "\nHello, "+user+". Let's play a quick game.<br>";
+    document.getElementById("QuizContent").innerHTML = "\nHello, "+user+". Let's play a quick game.<br />";
     Quiz("Did I grow up in Oregon?", "n", "I grew up in California.");
     if (cancelled != "Y") {
       Quiz("Did I complete the PHP Development track at Treehouse?", "y", "I passed the PHP Development track at Treehouse.");
@@ -73,7 +73,7 @@ function playGame() {
       NumQuiz(5);  
     }
     if (cancelled != "Y") {
-      document.getElementById("QuizContent").innerHTML += "<br>"+user+", you answered "+correct+" out of 3 biographical questions correctly and required "+numAttempts+" attempt(s) to guess my favorite number.";  
+      document.getElementById("QuizContent").innerHTML += "<br />"+user+", you answered "+correct+" out of 3 biographical questions correctly and required "+numAttempts+" attempt(s) to guess my favorite number.";  
     }
   } else {
     Cancel();
